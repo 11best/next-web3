9da7edf01a7e1285c96fed81ec5e660577c7ff00
best <best@biggestfan.net>
2022-08-31 17:25:41 +0700

    connect wallet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3 from "web3";
+import { useCallback, useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [wallet, setWallet] = useState("");
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is installed!");
+    }
+  }, []);
+
+  const onConnectClick = useCallback(async () => {
+    // let web3 = new Web3(Web3.givenProvider);
+    const acc = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    if (acc) {
+      setWallet(acc[0]);
+    }
+    console.log("connected =>", wallet);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +33,18 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <div>
+          <button
+            className="p-4 bg-slate-600 text-white rounded-lg"
+            onClick={() => onConnectClick()}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Connect wallet
+          </button>
+          <span className="text-slate-400 ml-2">{wallet}</span>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
